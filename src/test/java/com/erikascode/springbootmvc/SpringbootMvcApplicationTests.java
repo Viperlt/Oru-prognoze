45ae396f7a298e382cf8c2e2ed4761127e9a8016
Erikas Jurgelevicius <erikas.j1@gmail.com>
2023-03-13 09:08:57 +0200

    Programos pradzia

---
+++ src/test/java/com/erikascode/springbootmvc/SpringbootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erikascode.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
