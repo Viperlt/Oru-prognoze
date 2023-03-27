57cf57f47a38cc7b6fb74f3cdf6a1acb153bf933
Erikas Jurgelevicius <erikas.j1@gmail.com>
2023-03-27 08:54:37 +0300

    Basic hardcoded authentification

--- src/main/java/com/erikascode/springbootmvc/controllers/ForecastRestController.java
+++ src/main/java/com/erikascode/springbootmvc/controllers/ForecastRestController.java
@@ -1,6 +1,9 @@
 package com.erikascode.springbootmvc.controllers;
 
+import com.erikascode.springbootmvc.models.Forecast;
 import com.erikascode.springbootmvc.models.ForecastModel;
+import com.erikascode.springbootmvc.repositories.ForecastRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +12,18 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class ForecastRestController {
 
+    @Autowired
+    private ForecastRepository forecastRepository;
+
     @PostMapping(value = "/api/forecast", consumes = "application/json")
     public void forecastRestApi(@RequestBody ForecastModel model) {
+
+        Forecast entity = new Forecast(
+                model.data,
+                String.valueOf(model.temperatura),
+                "Vilnius",
+                1);
+        forecastRepository.save(entity);
 
     }
 }
--- src/main/java/com/erikascode/springbootmvc/controllers/MyForecastController.java
+++ src/main/java/com/erikascode/springbootmvc/controllers/MyForecastController.java
@@ -1,2 +1,38 @@
-package com.erikascode.springbootmvc.controllers;public class MyForecastController {
+package com.erikascode.springbootmvc.controllers;
+
+import com.erikascode.springbootmvc.models.Forecast;
+import com.erikascode.springbootmvc.models.IndexModel;
+import com.erikascode.springbootmvc.repositories.ForecastRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Controller
+public class MyForecastController {
+
+    @Autowired
+    private ForecastRepository forecastRepository;
+
+    @GetMapping("/my-forecasts")
+    public ModelAndView forecast(@RequestParam(required = false) String cityCode) throws IOException {
+        var modelAndView = new ModelAndView("myForecasts");
+        var model = forecastRepository.findAll();
+
+        modelAndView.addObject("myForecasts", model);
+        return modelAndView;
+    }
+
+//    @GetMapping("/delete-my-forecast")
+//    public void deleteForecast(@RequestParam(required = false) String cityCode, int id) throws IOException {
+//        var modelAndView = new ModelAndView("myForecasts");
+//        var model = forecastRepository.delete();
+//
+//
+//    }
+
 }
